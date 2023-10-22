3f005447111016ed1c7b29261b335a424e8f2781
Helina Allese <helinaallese@Helinas-MacBook-Air.local>
2023-10-22 10:45:30 +0300

    add week7 code

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Enter something:");
+string userInput = Console.ReadLine();
+
+PrintAnyWord(userInput); //userInput - argument
+
+
+
+static void PrintAnyWord(string anystring) //anyString - parameter
+{
+    anystring = anystring.ToUpper();
+    for(int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anystring);
+    }
+}
